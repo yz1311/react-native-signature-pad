f8e7ec49cb0432f87b97012e0bea92d7f2138395
David Khuu <dkhuu@rpmsoftware.com>
2017-03-24 14:41:43 -0600

    Change comment style from // to /* */
    Because the contents of 'content' is just the JS file wrapped in backticks, the // doesn't behave well in the Android WebView.
    
    #1

--- injectedJavaScript/signaturePad.js
+++ injectedJavaScript/signaturePad.js
@@ -45,7 +45,7 @@ function Bezier(startPoint, control1, control2, endPoint) {
   this.endPoint = endPoint;
 }
 
-// Returns approximated length.
+/* Returns approximated length. */
 Bezier.prototype.length = function () {
   var steps = 10;
   var length = 0;
@@ -92,8 +92,8 @@ function SignaturePad(canvas, options) {
   this._ctx = canvas.getContext('2d');
   this.clear();
 
-  // We need add these inline so they are available to unbind while still having
-  // access to 'self' we could use _.bind but it's not worth adding a dependency.
+  /* We need add these inline so they are available to unbind while still having
+      access to 'self' we could use _.bind but it's not worth adding a dependency. */
   this._handleMouseDown = function (event) {
     if (event.which === 1) {
       self._mouseButtonDown = true;
@@ -122,7 +122,7 @@ function SignaturePad(canvas, options) {
   };
 
   this._handleTouchMove = function (event) {
-    // Prevent scrolling.
+    /* Prevent scrolling. */
     event.preventDefault();
 
     var touch = event.targetTouches[0];
@@ -137,11 +137,11 @@ function SignaturePad(canvas, options) {
     }
   };
 
-  // Enable mouse and touch event handlers
+  /* Enable mouse and touch event handlers */
   this.on();
 }
 
-// Public methods
+/* Public methods */
 SignaturePad.prototype.clear = function () {
   var ctx = this._ctx;
   var canvas = this._canvas;
@@ -205,7 +205,7 @@ SignaturePad.prototype.isEmpty = function () {
   return this._isEmpty;
 };
 
-// Private methods
+/* Private methods */
 SignaturePad.prototype._strokeBegin = function (event) {
   this._data.push([]);
   this._reset();
@@ -259,7 +259,7 @@ SignaturePad.prototype._handleMouseEvents = function () {
 };
 
 SignaturePad.prototype._handleTouchEvents = function () {
-  // Pass touch events to canvas element on mobile IE11 and Edge.
+  /* Pass touch events to canvas element on mobile IE11 and Edge. */
   this._canvas.style.msTouchAction = 'none';
   this._canvas.style.touchAction = 'none';
 
@@ -288,8 +288,8 @@ SignaturePad.prototype._addPoint = function (point) {
   points.push(point);
 
   if (points.length > 2) {
-    // To reduce the initial lag make it work with 3 points
-    // by copying the first point to the beginning.
+    /* To reduce the initial lag make it work with 3 points
+        by copying the first point to the beginning. */
     if (points.length === 3) points.unshift(points[0]);
 
     tmp = this._calculateCurveControlPoints(points[0], points[1], points[2]);
@@ -299,8 +299,8 @@ SignaturePad.prototype._addPoint = function (point) {
     var curve = new Bezier(points[1], c2, c3, points[2]);
     var widths = this._calculateCurveWidths(curve);
 
-    // Remove the first element from the list,
-    // so that we always have no more than 4 points in points array.
+    /* Remove the first element from the list,
+        so that we always have no more than 4 points in points array. */
     points.shift();
 
     return { curve: curve, widths: widths };
@@ -374,7 +374,7 @@ SignaturePad.prototype._drawCurve = function (curve, startWidth, endWidth) {
   ctx.beginPath();
 
   for (var i = 0; i < drawSteps; i += 1) {
-    // Calculate the Bezier (x, y) coordinate for this step.
+    /* Calculate the Bezier (x, y) coordinate for this step. */
     var t = i / drawSteps;
     var tt = t * t;
     var ttt = tt * t;
@@ -420,11 +420,11 @@ SignaturePad.prototype._fromData = function (pointGroups, drawCurve, drawDot) {
         var point = new Point(rawPoint.x, rawPoint.y, rawPoint.time);
 
         if (j === 0) {
-          // First point in a group. Nothing to draw yet.
+          /* First point in a group. Nothing to draw yet. */
           this._reset();
           this._addPoint(point);
         } else if (j !== group.length - 1) {
-          // Middle point in a group.
+          /* Middle point in a group. */
           var _addPoint2 = this._addPoint(point),
               curve = _addPoint2.curve,
               widths = _addPoint2.widths;
@@ -433,7 +433,7 @@ SignaturePad.prototype._fromData = function (pointGroups, drawCurve, drawDot) {
             drawCurve(curve, widths);
           }
         } else {
-          // Last point in a group. Do nothing.
+          /* Last point in a group. Do nothing. */
         }
       }
     } else {
@@ -462,9 +462,9 @@ SignaturePad.prototype._toSVG = function () {
   this._fromData(pointGroups, function (curve, widths) {
     var path = document.createElement('path');
 
-    // Need to check curve for NaN values, these pop up when drawing
-    // lines on the canvas that are not continuous. E.g. Sharp corners
-    // or stopping mid-stroke and than continuing without lifting mouse.
+    /* Need to check curve for NaN values, these pop up when drawing
+        lines on the canvas that are not continuous. E.g. Sharp corners
+        or stopping mid-stroke and than continuing without lifting mouse. */
     if (!isNaN(curve.control1.x) && !isNaN(curve.control1.y) && !isNaN(curve.control2.x) && !isNaN(curve.control2.y)) {
       var attr = 'M ' + curve.startPoint.x.toFixed(3) + ',' + curve.startPoint.y.toFixed(3) + ' ' + ('C ' + curve.control1.x.toFixed(3) + ',' + curve.control1.y.toFixed(3) + ' ') + (curve.control2.x.toFixed(3) + ',' + curve.control2.y.toFixed(3) + ' ') + (curve.endPoint.x.toFixed(3) + ',' + curve.endPoint.y.toFixed(3));
 
@@ -491,7 +491,7 @@ SignaturePad.prototype._toSVG = function () {
   var header = '<svg' + ' xmlns="http://www.w3.org/2000/svg"' + ' xmlns:xlink="http://www.w3.org/1999/xlink"' + (' viewBox="' + minX + ' ' + minY + ' ' + maxX + ' ' + maxY + '"') + (' width="' + maxX + '"') + (' height="' + maxY + '"') + '>';
   var body = svg.innerHTML;
 
-  // IE hack for missing innerHTML property on SVGElement
+  /* IE hack for missing innerHTML property on SVGElement */
   if (body === undefined) {
     var dummy = document.createElement('dummy');
     var nodes = svg.childNodes;
