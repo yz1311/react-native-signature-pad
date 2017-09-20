998978360e8595dacdd5dcb4ccc052d00c234e4c
David Khuu <dkhuu@rpmsoftware.com>
2017-09-20 15:28:47 -0600

    Use window.postMessage to relay a finished stroke back to the app
    With changes to Android 7 and how the default WebView in apps changed from Google WebView APK to Chrome WebView APK, we have to change how the data is sent from the WebView to the app as the previous method does not work on Chrome WebView
    
    #14

--- index.js
+++ index.js
@@ -161,11 +161,17 @@ class SignaturePad extends Component {
   _renderLoading = (args) => {
   };
 
+  _onMessage = (event) => {
+    var base64DataUrl = JSON.parse(event.nativeEvent.data);
+    this._bridged_finishedStroke(base64DataUrl);
+  }
+
   render = () => {
     return (
       <WebView
         automaticallyAdjustContentInsets={false}
         onNavigationStateChange={this._onNavigationChange}
+        onMessage={this._onMessage}
         renderError={this._renderError}
         renderLoading={this._renderLoading}
         source={this.source}
